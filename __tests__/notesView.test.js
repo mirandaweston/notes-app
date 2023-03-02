f4032df8946eb48b82c4e3bfd5285552c12184e5
mirandaweston <mweston105@yahoo.co.uk>
2023-03-02 12:07:39 +0000

    Updating NotesView test suite: passes 'addNewNote(newNote)' method and updates 'displayNotes' method

--- __tests__/notesView.test.js
+++ __tests__/notesView.test.js
@@ -20,4 +20,34 @@ describe('Notes view', () => {
 
     expect(document.querySelectorAll('div.note').length).toEqual(2);
   });
+
+  it('adds a new note', () => {
+    document.body.innerHTML = fs.readFileSync('./index.html');
+
+    const model = new NotesModel();
+    const view = new NotesView(model);
+
+    const input = document.querySelector('#add-note-input');
+    input.value = 'My new test note';
+
+    const button = document.querySelector('#add-note-btn');
+    button.click();
+
+    expect(document.querySelectorAll('div.note').length).toEqual(1);
+    expect(document.querySelectorAll('div.note')[0].textContent).toEqual('My new test note');
+  })
+
+  it('clears the list of previous notes before displaying', () => {
+    document.body.HTML = fs.readFileSync('./index.html');
+
+    const model = new NotesModel();
+    const view = new NotesView(model);
+    model.addNote('Note one');
+    model.addNote('Node two');
+
+    view.displayNotes();
+    view.displayNotes();
+
+    expect(document.querySelectorAll('div.note').length).toEqual(2);
+  });
 })
