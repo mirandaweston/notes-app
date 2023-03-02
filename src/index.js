0cf0661339e420797aecc07b647a1db6ab27caca
mirandaweston <mweston105@yahoo.co.uk>
2023-03-02 12:14:41 +0000

    Updating main file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 const NotesModel = require('./notesModel');
 const NotesView = require('./notesView')
 
-let model = new NotesModel;
+let notemodel = new NotesModel;
+notemodel.addNote('Buy Milk');
+notemodel.addNote('Go to the Gym');
+console.log(notemodel.getNotes());
 
+const model = new NotesModel;
 model.addNote('This is an example note');
 
-let view = new NotesView(model);
-
+const view = new NotesView(model);
 view.displayNotes();
